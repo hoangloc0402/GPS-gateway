bbae4dbe3cc7f080c0598f9e05cc2c86e12955ac
QuocBao <quocbao100@gmail.com>
2017-09-25 14:38:47 +0700

    add UDP

--- src/com/hoangloc0402/gps/gateway/Gateway.java
+++ src/com/hoangloc0402/gps/gateway/Gateway.java
@@ -2,23 +2,35 @@ package com.hoangloc0402.gps.gateway;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Calendar;
+
 public class Gateway {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		DatagramSocket serverSocket = new DatagramSocket(9876);
+		byte[] receiveData = new byte[1024];
+		byte[] sendData ;
+
 		Publisher publisher = new Publisher("[Publisher]");
 		publisher.connect();
-
-		int i=0;
+		System.out.println("Gateway is running...");
+		long preTime = Calendar.getInstance().getTimeInMillis();
+		long curTime;
 		try {
 			while (true) {
-				try {
-					Thread.sleep(2000);
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				serverSocket.receive(receivePacket);
+
+				String sentence = new String(receivePacket.getData(),receivePacket.getOffset(),receivePacket.getLength());
+				System.out.println(sentence);
+				publisher.addMessage(sentence);
+				curTime = Calendar.getInstance().getTimeInMillis();
+				if(curTime - preTime > 10000) {
+					preTime = curTime;
 					if (publisher.isConnected()) {
-						publisher.addMessage("Fuck you " + i); i++;
-						publisher.publishTextMessage((publisher.getMessage()));
+						if(!publisher.PublishQueue.isEmpty()) publisher.publishTextMessage((publisher.getMessage()));
 					}
-				}
-				catch (Exception e) {
-					e.printStackTrace();
 				}
 			}
 		}
